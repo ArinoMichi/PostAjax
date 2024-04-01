67e556a2a05f70dc0ea986e0ffef99994a9e41f4
ArinoMichi <cutesyprogrammer@gmail.com>
2024-04-01 10:14:07 +0200

    Add project files.

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostAjax.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private static List<string> _productos = new List<string>
+        {
+            "Pan",
+            "Té verde",
+            "Galletas"
+        };
+
+        public IActionResult Index()
+        {
+            return View(_productos);
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(string nombre)
+        {
+            _productos.Add(nombre);
+            return Json(new { success = true });
+        }
+        [HttpPost]
+        public IActionResult Eliminar(string nombre)
+        {
+            _productos.Remove(nombre);
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PostTransiciones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
